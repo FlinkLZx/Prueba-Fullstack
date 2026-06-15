ceffea50e4ef9172a02f7692a389b324141f2d32
FlinkLZx <juanfeartunduaga914@gmail.com>
2026-06-15 18:30:37 -0500

    feature: created Category

---
+++ src/main/java/com/example/convocatoriasBackend/category/domain/model/Category.java
@@ -0,0 +1,13 @@
+package com.example.convocatoriasBackend.category.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Category {
+    private Long id;
+    private String name;
+}
